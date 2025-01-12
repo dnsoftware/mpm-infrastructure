03e2138d75a16e8cdaff25dcdc38b0f78575a235
Dmitry <ddaemon@mail.ru>
2025-01-12 13:24:08 +0300

    patch readonly tables

---
+++ clickhouse/clickhouse-docker/clear_databases.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -f -r /home/dmitry/projects/mpmsystem/infrastructure/clickhouse/clickhouse-docker/clickhouse/clickhouse1/data/*
+rm -f -r /home/dmitry/projects/mpmsystem/infrastructure/clickhouse/clickhouse-docker/clickhouse/clickhouse2/data/*
+rm -f -r /home/dmitry/projects/mpmsystem/infrastructure/clickhouse/clickhouse-docker/clickhouse/clickhouse3/data/*
+
+rm -f -r /home/dmitry/projects/mpmsystem/infrastructure/clickhouse/clickhouse-docker/clickhouse/clickhouse1/logs/*
+rm -f -r /home/dmitry/projects/mpmsystem/infrastructure/clickhouse/clickhouse-docker/clickhouse/clickhouse2/logs/*
+rm -f -r /home/dmitry/projects/mpmsystem/infrastructure/clickhouse/clickhouse-docker/clickhouse/clickhouse3/logs/*
+
+rm -f -r /home/dmitry/projects/mpmsystem/infrastructure/clickhouse/clickhouse-docker/zookeeper/zookeeper1/data/*
+rm -f -r /home/dmitry/projects/mpmsystem/infrastructure/clickhouse/clickhouse-docker/zookeeper/zookeeper2/data/*
+rm -f -r /home/dmitry/projects/mpmsystem/infrastructure/clickhouse/clickhouse-docker/zookeeper/zookeeper3/data/*
+
+rm -f -r /home/dmitry/projects/mpmsystem/infrastructure/clickhouse/clickhouse-docker/zookeeper/zookeeper1/datalog/*
+rm -f -r /home/dmitry/projects/mpmsystem/infrastructure/clickhouse/clickhouse-docker/zookeeper/zookeeper2/datalog/*
+rm -f -r /home/dmitry/projects/mpmsystem/infrastructure/clickhouse/clickhouse-docker/zookeeper/zookeeper3/datalog/*
